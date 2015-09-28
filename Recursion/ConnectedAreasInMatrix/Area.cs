6601efbc749eee92e25ee7c65f1506e142caba83
Stayko Nedev <zakkgard@gmail.com>
2015-09-28 22:46:39 +0300

    added connected areas problem solution

---
+++ Recursion/ConnectedAreasInMatrix/Area.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectedAreasInMatrix
+{
+    public class Area : IComparable<Area>
+    {
+        public Area(int size, int row, int col)
+        {
+            this.Size = size;
+            this.Row = row;
+            this.Col = col;
+        }
+
+        public int Size { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        public int CompareTo(Area other)
+        {
+            int compareSize = -1 * this.Size.CompareTo(other.Size);
+            if (compareSize == 0)
+            {
+                int compareRows = this.Row.CompareTo(other.Row);
+                if (compareRows == 0)
+                {
+                    return this.Col.CompareTo(other.Col);
+                }
+                else
+                {
+                    return compareRows;
+                }
+            }
+            else
+            {
+                return compareSize;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+                            "({0}, {1}), size: {2}",
+                            this.Row,
+                            this.Col,
+                            this.Size);
+        }
+    }
+}
