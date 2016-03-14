0483806ce067de8543ebfe0aee9e86dd09ad75d6
Stayko Nedev <Zakkgard@users.noreply.github.com>
2016-03-14 21:19:04 +0200

    Implemented HashTable with chaining

--- DictionariesAndHashtables/HashTable/KeyValue.cs
+++ DictionariesAndHashtables/HashTable/KeyValue.cs
@@ -2,14 +2,14 @@
 
 public class KeyValue<TKey, TValue>
 {
-    public TKey Key { get; set; }
-    public TValue Value { get; set; }
-
     public KeyValue(TKey key, TValue value)
     {
         this.Key = key;
         this.Value = value;
     }
+
+    public TKey Key { get; set; }
+    public TValue Value { get; set; }
 
     public override bool Equals(object other)
     {
@@ -23,13 +23,13 @@ public class KeyValue<TKey, TValue>
         return this.CombineHashCodes(this.Key.GetHashCode(), this.Value.GetHashCode());
     }
 
-    private int CombineHashCodes(int h1, int h2)
-    {
-        return ((h1 << 5) + h1) ^ h2;
-    }
-
     public override string ToString()
     {
         return string.Format(" [{0} -> {1}]", this.Key, this.Value);
+    }
+
+    private int CombineHashCodes(int h1, int h2)
+    {
+        return ((h1 << 5) + h1) ^ h2;
     }
 }
