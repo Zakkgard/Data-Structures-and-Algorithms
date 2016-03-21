febd0b4561d053b1e3ddda2992379b7ef656f378
Stayko Nedev <zakkgard@gmail.com>
2016-03-21 22:32:24 +0200

    implemented an AVL tree

--- Advanced-Tree-Structures/Advanced-Tree-Structures/Lab/AvlTree/AvlTree.cs
+++ Advanced-Tree-Structures/Advanced-Tree-Structures/Lab/AvlTree/AvlTree.cs
@@ -3,6 +3,7 @@
     using System;
     
     public class AvlTree<T>
+        where T : IComparable<T>
     {
         private Node<T> root;
 
@@ -10,17 +11,225 @@
         
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            var inserted = true;
+            if (this.root == null)
+            {
+                this.root = new Node<T>(item);
+            }
+            else
+            {
+                inserted = this.InsertInternal(this.root, item);
+            }
+
+            if (inserted)
+            {
+                this.Count++;
+            }
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            var node = this.root;
+
+            while (node != null)
+            {
+                if (node.Value.CompareTo(item) < 0)
+                {
+                    node = node.RightChild;
+                }
+                else if (node.Value.CompareTo(item) > 0)
+                {
+                    node = node.LeftChild;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public void ForeachDfs(Action<int, T> action)
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                return;
+            }
+
+            this.InOrderDfs(this.root, 1, action);
+        }
+
+        private bool InsertInternal(Node<T> node, T item)
+        {
+            var currentNode = node;
+            var newNode = new Node<T>(item);
+            var shouldRetrace = false;
+            while (true)
+            {
+                if (currentNode.Value.CompareTo(item) < 0)
+                {
+                    if (currentNode.RightChild == null)
+                    {
+                        currentNode.RightChild = newNode;
+                        currentNode.BalanceFactor--;
+                        shouldRetrace = currentNode.BalanceFactor != 0;
+                        break;
+                    }
+
+                    currentNode = currentNode.RightChild;
+                }
+                else if (currentNode.Value.CompareTo(item) > 0)
+                {
+                    if (currentNode.LeftChild == null)
+                    {
+                        currentNode.LeftChild = newNode;
+                        currentNode.BalanceFactor++;
+                        shouldRetrace = currentNode.BalanceFactor != 0;
+                        break;
+                    }
+
+                    currentNode = currentNode.LeftChild;
+                }
+
+                return false;
+            }
+
+            if (shouldRetrace)
+            {
+                this.RetraceInsert(currentNode);
+            }
+
+            return true;
+        }
+
+        private void RetraceInsert(Node<T> node)
+        {
+            var parent = node.Parent;
+            while (parent != null)
+            {
+                if (node.IsLeftChild)
+                {
+                    if (parent.BalanceFactor == 1)
+                    {
+                        parent.BalanceFactor++;
+                        if (node.BalanceFactor == -1)
+                        {
+                            this.RotateLeft(node);
+                        }
+
+                        this.RotateRight(parent);
+                        break;
+                    }
+                    else if (parent.BalanceFactor == -1)
+                    {
+                        parent.BalanceFactor = 0;
+                        break;
+                    }
+                    else
+                    {
+                        parent.BalanceFactor = 1;
+                    }
+                }
+                else
+                {
+                    if (parent.BalanceFactor == -1)
+                    {
+                        parent.BalanceFactor--;
+                        if (node.BalanceFactor == 1)
+                        {
+                            this.RotateRight(node);
+                        }
+
+                        this.RotateLeft(parent);
+                        break;
+                    }
+                    else if (parent.BalanceFactor == 1)
+                    {
+                        parent.BalanceFactor = 0;
+                        break;
+                    }
+                    else
+                    {
+                        parent.BalanceFactor = -1;
+                    }
+                }
+
+                node = parent;
+                parent = node.Parent;
+            }
+        }
+
+        private void RotateRight(Node<T> node)
+        {
+            var parent = node.Parent;
+            var child = node.LeftChild;
+
+            if (parent != null)
+            {
+                if (node.IsRightChild)
+                {
+                    parent.RightChild = child;
+                }
+                else
+                {
+                    parent.LeftChild = child;
+                }
+            }
+            else
+            {
+                this.root = child;
+                this.root.Parent = null;
+            }
+
+            node.LeftChild = child.RightChild;
+            child.RightChild = node;
+
+            node.BalanceFactor -= 1 + Math.Max(child.BalanceFactor, 0);
+            child.BalanceFactor -= 1 - Math.Min(node.BalanceFactor, 0);
+        }
+
+        private void RotateLeft(Node<T> node)
+        {
+            var parent = node.Parent;
+            var child = node.RightChild;
+            if (parent != null)
+            {
+                if (node.IsLeftChild)
+                {
+                    parent.LeftChild = child;
+                }
+                else
+                {
+                    parent.RightChild = child;
+                }
+            }
+            else
+            {
+                this.root = child;
+                this.root.Parent = null;
+            }
+
+            node.RightChild = child.LeftChild;
+            child.LeftChild = node;
+
+            node.BalanceFactor += 1 - Math.Min(child.BalanceFactor, 0);
+            child.BalanceFactor += 1 + Math.Max(node.BalanceFactor, 0);
+        }
+
+        private void InOrderDfs(Node<T> node, int depth, Action<int, T> action)
+        {
+            if (node.LeftChild != null)
+            {
+                this.InOrderDfs(node.LeftChild, depth + 1, action);
+            }
+
+            action(depth, node.Value);
+
+            if (node.RightChild != null)
+            {
+                this.InOrderDfs(node.RightChild, depth + 1, action);
+            }
         }
     }
 }
--- Advanced-Tree-Structures/Advanced-Tree-Structures/Lab/AvlTree/Node.cs
+++ Advanced-Tree-Structures/Advanced-Tree-Structures/Lab/AvlTree/Node.cs
@@ -1,7 +1,98 @@
-﻿namespace AvlTreeLab
+﻿using System;
+
+namespace AvlTreeLab
 {
     public class Node<T>
+        where T: IComparable<T>
     {
+        private Node<T> leftChild;
+        private Node<T> rightChild;
+
+        public Node(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public Node<T> LeftChild
+        {
+            get
+            {
+                return this.leftChild;
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    value.Parent = this;
+                }
+
+                this.leftChild = value;
+            }
+        }
+
+        public Node<T> RightChild
+        {
+            get
+            {
+                return this.rightChild;
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    value.Parent = this;
+                }
+
+                this.rightChild = value;
+            }
+        }
+
+        public Node<T> Parent { get; set; }
+
+        public int BalanceFactor { get; set; }
+
+        public bool IsLeftChild
+        {
+            get
+            {
+                if (this.Parent != null && this.Parent.LeftChild != null)
+                {
+                    if (this.Parent.LeftChild == this)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        public bool IsRightChild
+        {
+            get
+            {
+                if (this.Parent != null && this.Parent.RightChild != null)
+                {
+                    if (this.Parent.RightChild == this)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        public int ChildrenCount { get; set; }
+
+        public override string ToString()
+        {
+            return this.Value.ToString();
+        }
     }
 }
 
