aa3c9e332314da3d02baef07ea2afbb7e12ed878
Stayko Nedev <zakkgard@gmail.com>
2016-03-26 17:47:50 +0200

    collection of products (unfinished)

---
+++ Data-Structure-Efficiency/Homework/CollectionOfProducts/ProductCollection.cs
@@ -0,0 +1,26 @@
+﻿namespace CollectionOfProducts
+{
+    using System.Collections.Generic;
+
+    using Wintellect.PowerCollections;
+
+    public class ProductCollection
+    {
+        private Dictionary<int, Product> byId;
+        private Dictionary<string, SortedSet<Product>> byTitle;
+        private Dictionary<string, SortedSet<Product>> byTitlePrice;
+        private Dictionary<string, OrderedDictionary<decimal, SortedSet<Product>>> byTitlePriceRange;
+        private Dictionary<string, SortedSet<Product>> bySupplierPrice;
+        private Dictionary<string, OrderedDictionary<decimal, SortedSet<Product>>> bySupplierPriceRange;
+
+        public ProductCollection()
+        {
+            this.byId = new Dictionary<int, Product>();
+            this.bySupplierPrice = new Dictionary<string, SortedSet<Product>>();
+            this.bySupplierPriceRange = new Dictionary<string, OrderedDictionary<decimal, SortedSet<Product>>>();
+            this.byTitle = new Dictionary<string, SortedSet<Product>>();
+            this.byTitlePrice = new Dictionary<string, SortedSet<Product>>();
+            this.byTitlePriceRange = new Dictionary<string, OrderedDictionary<decimal, SortedSet<Product>>>();
+        }
+    }
+}
---
+++ Data-Structure-Efficiency/Homework/CollectionOfProducts/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CollectionOfProducts
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
