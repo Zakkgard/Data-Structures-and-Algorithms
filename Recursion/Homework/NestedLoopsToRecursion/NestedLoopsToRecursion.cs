a678eda72463ffbe00d959cb9575cdbc36a7409d
Stayko Nedev <zakkgard@gmail.com>
2016-04-11 20:50:59 +0300

    nested loops to recursion & reverse array problems

---
+++ Recursion/Homework/NestedLoopsToRecursion/NestedLoopsToRecursion.cs
@@ -0,0 +1,43 @@
+﻿namespace NestedLoopsToRecursion
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class NestedLoopsToRecursion
+    {
+        static int n;
+        static int[] array;
+
+        public static void Main()
+        {
+            n = int.Parse(Console.ReadLine());
+            array = new int[n];
+
+            SimulateLoops(0);
+        }
+
+        private static void SimulateLoops(int loop)
+        {
+            if (loop == n)
+            {
+                Print();
+                return;
+            }
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                array[loop] = i;
+                SimulateLoops(loop + 1);
+            }
+        }
+
+        private static void Print()
+        {
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
---
+++ Recursion/Homework/ReverseArray/ReverseArray.cs
@@ -0,0 +1,37 @@
+﻿namespace ReverseArray
+{
+    using System;
+    using System.Linq;
+
+    public class ReverseArray
+    {
+        public static void Main()
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Reverse(array, 0);
+            Print(array);
+            Console.ReadLine();
+        }
+
+        private static void Reverse(int[] array, int index)
+        {
+            if (index > array.Length / 2 - 1)
+            {
+                return;
+            }
+
+            Reverse(array, index + 1);
+
+            int length = array.Length;
+            int temp = array[index];
+            array[index] = array[length - index - 1];
+            array[length - index - 1] = temp;
+        }
+
+        private static void Print(int[] array)
+        {
+            Console.WriteLine(string.Join(",", array));
+        }
+    }
+}
